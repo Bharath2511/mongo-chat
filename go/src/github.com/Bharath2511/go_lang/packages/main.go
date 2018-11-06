96c401f0c6a3af2d4acd09a72bdf7a4144ea4c56
V S Bharath <bharathcse5t6@gmail.com>
2018-11-06 12:57:58 +0530

    importing packages

--- go/src/github.com/Bharath2511/go_lang/packages/main.go
+++ go/src/github.com/Bharath2511/go_lang/packages/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt" 
 	"math"
+	"github.com/Bharath2511/go_lang/packages/strutils"
 )
 
 func main() {
   fmt.Println(math.Floor(2.7))
   fmt.Println(math.Ceil(2.7))
   fmt.Println(math.Sqrt(36))
+  fmt.Println(strutils.Reverse("World"))
 }
